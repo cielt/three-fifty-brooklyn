6c879fc533d385f48d70564b59dd083e38882503
leslie tu <chiayin.l.tu@gmail.com>
2019-03-25 10:24:24 -0400

    [WIP] build out home page template, cont'd * add custom font files * add CSS for sign up form, events feed * update Gulp tasks to include autoprefixing

--- utils/gulpfile.js
+++ utils/gulpfile.js
@@ -1,8 +1,9 @@
 const gulp = require('gulp');
 const { src, dest, series, watch } = require('gulp');
-var sass = require('gulp-sass');
-var gulpIf = require('gulp-if')
-var cssNano = require('gulp-cssnano');
+var sass = require('gulp-sass'),
+    gulpIf = require('gulp-if'),
+    cssNano = require('gulp-cssnano'),
+    autoprefixer = require('gulp-autoprefixer');
 
 sass.compiler = require('node-sass');
 
@@ -15,6 +16,10 @@ function compileSass() {
   return src('../scss/style.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(gulpIf('*.css', cssNano()))
+  .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
   .pipe(dest('../'));
 }
 
